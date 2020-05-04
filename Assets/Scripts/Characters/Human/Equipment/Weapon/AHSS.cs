23c2c2e953643cc3a8c93c60b7f78cc74a79dd0a
AlexWolski <alex.theinfinite@gmail.com>
2020-05-03 22:05:45 -0700

    Added class skeletons for the equipment classes

---
+++ Assets/Scripts/Characters/Human/Equipment/Weapon/AHSS.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class AHSS : OdmgEquipment, Weapon
+{
+    [SerializeField] private GameObject myHero;
+    private Hero myHeroScript;
+
+    [SerializeField] private int maxAmmo = 7;
+    public int leftGunAmmo { get; private set; }
+    public int rightGunAmmo { get; private set; }
+
+    protected override void Awake()
+    {
+        myHeroScript = myHero.GetComponent<Hero>();
+        base.Initialize(myHeroScript);
+
+        leftGunAmmo = maxAmmo;
+        rightGunAmmo = maxAmmo;
+    }
+
+    protected override void Update()
+    {
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Reload()
+    {
+
+    }
+
+    public new void Resupply()
+    {
+        base.Resupply();
+    }
+}
---
+++ Assets/Scripts/Characters/Human/Equipment/Weapon/Blades.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Blades : OdmgEquipment, Weapon
+{
+    [SerializeField] private GameObject myHero;
+    private Hero myHeroScript;
+
+    [SerializeField] private int maxBlades = 5;
+    [SerializeField] private float maxBladeDurability = 100f;
+    public int NumBlades { get; private set; }
+    public float BladeDurability { get; private set; }
+
+    protected override void Awake()
+    {
+        myHeroScript = myHero.GetComponent<Hero>();
+        base.Initialize(myHeroScript);
+
+        NumBlades = maxBlades;
+        BladeDurability = maxBladeDurability;
+    }
+
+    protected override void Update()
+    {
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Reload()
+    {
+
+    }
+
+    public new void Resupply()
+    {
+        base.Resupply();
+    }
+}
