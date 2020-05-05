bdb3de52e2e37837cd3f42c137b06f8fc79afe59
AlexWolski <alex.theinfinite@gmail.com>
2020-05-05 02:47:31 -0700

    Moved gas consumption into ODMG class and added SetStats method for equipment

--- Assets/Scripts/Characters/Human/Equipment/Weapon/Blades.cs
+++ Assets/Scripts/Characters/Human/Equipment/Weapon/Blades.cs
@@ -3,24 +3,43 @@
 public class Blades : OdmgEquipment, Weapon
 {
     [SerializeField] private int maxBlades = 5;
-    [SerializeField] private float maxBladeDurability = 100f;
+    [SerializeField] private float maxDurability = 100f;
     public int NumBlades { get; private set; }
     public float BladeDurability { get; private set; }
 
     protected override void Awake()
     {
         NumBlades = maxBlades;
-        BladeDurability = maxBladeDurability;
+        BladeDurability = maxDurability;
     }
 
     protected override void Update()
     {
     }
 
-    public void Resupply()
+    #region Equipment Methods
+    public override void SetStats(HeroStat heroStat)
     {
-
+        base.SetStats(heroStat);
+        maxBlades = heroStat.BLA;
+        NumBlades = maxBlades;
     }
+
+    public override bool NeedResupply()
+    {
+        if (base.NeedResupply() || NumBlades != maxBlades || BladeDurability != maxDurability)
+            return true;
+
+        return false;
+    }
+
+    public override void Resupply()
+    {
+        base.Resupply();
+        NumBlades = maxBlades;
+        BladeDurability = maxDurability;
+    }
+    #endregion
 
     #region Weapon Methods
     public void Attack()
